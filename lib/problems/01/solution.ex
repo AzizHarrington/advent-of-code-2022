e6225b07eec5e02efe6c8486126889bb60ccce3f
AzizHarrington <azizharrington@gmail.com>
2022-12-04 23:50:31 -0600

    Problem 1

---
+++ lib/problems/01/solution.ex
@@ -0,0 +1,25 @@
+defmodule Problems.Solution do
+  def run(filename) do
+    File.read!(filename)
+    |> String.split("\n")
+    |> Stream.chunk_while([], &chunk_fun/2, &after_fun/1)
+    |> Stream.map(&parse_integer_string_list/1)
+    |> Stream.map(&Enum.sum/1)
+    |> Enum.to_list()
+    |> Enum.max()
+  end
+
+  defp chunk_fun("", acc), do: {:cont, Enum.reverse(acc), []}
+  defp chunk_fun(element, acc), do: {:cont, [element | acc]}
+
+  defp after_fun([]), do: {:cont, []}
+  defp after_fun(acc), do: {:cont, Enum.reverse(acc), []}
+
+  defp parse_integer_string_list(list) do
+    list
+    |> Enum.map(fn integer_string ->
+      {result, _} = Integer.parse(integer_string)
+      result
+    end)
+  end
+end
