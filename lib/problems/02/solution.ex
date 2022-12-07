28cea3bb4d3770cc64c1a9f9ca669605993804f0
AzizHarrington <azizharrington@gmail.com>
2022-12-07 13:59:02 -0600

    Day 2 part 2 solution

--- lib/problems/02/solution.ex
+++ lib/problems/02/solution.ex
@@ -2,30 +2,43 @@ defmodule Problems.Solution2 do
   def run(filename) do
     load_file(filename)
     |> Stream.map(&(String.split(&1, " ")))
-    |> Stream.map(fn [opponent_code, player_code] ->
-      [object(opponent_code), object(player_code)]
+    |> Stream.map(fn [opponent_code, outcome_code] ->
+      [object(opponent_code), outcome(outcome_code)]
     end)
-    |> Stream.map(fn [opponent, player] ->
-      object_score(player) + match_score(player, opponent)
+    |> Stream.map(fn [opponent, outcome] ->
+      player = object_for_outcome(opponent, outcome)
+      object_score(player) + match_score(outcome)
     end)
     |> Enum.sum()
   end
 
-  defp object(code) when code in ["A", "X"], do: :rock
-  defp object(code) when code in ["B", "Y"], do: :paper
-  defp object(code) when code in ["C", "Z"], do: :scissors
+  defp object("A"), do: :rock
+  defp object("B"), do: :paper
+  defp object("C"), do: :scissors
+
+  defp outcome("X"), do: :lose
+  defp outcome("Y"), do: :draw
+  defp outcome("Z"), do: :win
+
+  defp object_for_outcome(:rock, :lose), do: :scissors
+  defp object_for_outcome(:rock, :draw), do: :rock
+  defp object_for_outcome(:rock, :win), do: :paper
+
+  defp object_for_outcome(:paper, :lose), do: :rock
+  defp object_for_outcome(:paper, :draw), do: :paper
+  defp object_for_outcome(:paper, :win), do: :scissors
+
+  defp object_for_outcome(:scissors, :lose), do: :paper
+  defp object_for_outcome(:scissors, :draw), do: :scissors
+  defp object_for_outcome(:scissors, :win), do: :rock
 
   defp object_score(:rock), do: 1
   defp object_score(:paper), do: 2
   defp object_score(:scissors), do: 3
 
-  defp match_score(:rock, :scissors), do: 6
-  defp match_score(:paper, :rock), do: 6
-  defp match_score(:scissors, :paper), do: 6
-  defp match_score(:scissors, :rock), do: 0
-  defp match_score(:rock, :paper), do: 0
-  defp match_score(:paper, :scissors), do: 0
-  defp match_score(object, object), do: 3
+  defp match_score(:win), do: 6
+  defp match_score(:draw), do: 3
+  defp match_score(:lose), do: 0
 
   defp load_file(filename) do
     File.read!(filename)
