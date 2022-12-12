0a66571fe4dbf1756ceb1bfb7b0ba9387e8245d8
AzizHarrington <azizharrington@gmail.com>
2022-12-11 22:51:02 -0600

    Day 7 part 2

--- lib/problems/07/solution.ex
+++ lib/problems/07/solution.ex
@@ -5,13 +5,78 @@ defmodule Problems.Solution7 do
     file = load_file(filename)
 
     IO.inspect part1(file)
+    IO.inspect part2(file)
   end
 
   defp part1(file) do
+    file
+    |> build_file_system()
+    |> find_directory_sizes()
+    |> Enum.map(fn {size, _ } -> size end)
+    |> Enum.filter(fn size -> size <= 100_000 end)
+    |> Enum.sum()
+  end
+
+  defp part2(file) do
+    [ root | directories ] =
+      file
+        |> build_file_system()
+        |> find_directory_sizes()
+        |> Enum.map(fn {size, _ } -> size end)
+        |> Enum.sort(:desc)
+
+    directories
+      |> Enum.filter(fn dir ->
+        (70_000_000 - root + dir) > 30_000_000
+      end)
+      |> Enum.min()
+  end
+
+  defp find_directory_sizes(tree) do
+    tree
+    |> directory_sizes()
+    |> Enum.map(fn {directory, size, descendent_size } ->
+      {size + descendent_size, directory}
+    end)
+  end
+
+  defp directory_sizes(tree) when tree == %{} do
+    []
+  end
+
+  defp directory_sizes(tree) do
+    tree
+    |> Enum.flat_map(fn {dir, %{children: children, files: files}} ->
+      descendents = directory_sizes(children)
+
+      dir_content_size =
+        files
+        |> Enum.map(fn {_, size} -> size end)
+        |> Enum.sum()
+
+      dir_size = {
+        dir,
+        dir_content_size,
+        get_descendent_size(descendents)
+      }
+
+      [dir_size | descendents]
+    end)
+  end
+
+  defp get_descendent_size([]), do: 0
+  defp get_descendent_size([ {_, size, _} |  descendents]) do
+    size + get_descendent_size(descendents)
+  end
+
+  defp build_file_system(file) do
     commands = file |> Enum.map(&String.split/1)
 
-
     file_system = build(%{"/" => %{files: [], children: %{}}}, [], commands)
+
+    File.open("./day7tree", [:write], fn f ->
+      IO.inspect(f, file_system, [])
+    end)
 
     file_system
   end
@@ -37,7 +102,8 @@ defmodule Problems.Solution7 do
   end
 
   defp build(tree, path, [[size, filename] | commands]) do
-    updated_tree = add_file(tree, Enum.reverse(path), filename, size)
+    {parsed_size, _} = Integer.parse(size)
+    updated_tree = add_file(tree, Enum.reverse(path), filename, parsed_size)
 
     build(updated_tree, path, commands)
   end
